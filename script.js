5bb3507a9032c9e41e0ff99ea32e7a6ec96f0eb8
Vipul Dixit <dixit.vipul0511@gmail.com>
2024-01-04 16:01:20 +0530

    Update script.js for error handling

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("connected");
 const api = "b7ccf0dd3aae4bf7ba0135554230710";
 const textContent = document.querySelector(".text-content");
 const searchBox = document.querySelector("#searchBox");
@@ -11,10 +10,10 @@ async function result(location) {
   let call = `https://api.weatherapi.com/v1/current.json?key=${api}&q=${location}`;
   let result = await fetch(call);
   let data = await result.json();
-  const icon = data.current.condition.icon;
-  console.log(icon);
-  weathericon.innerHTML = `<img src="https:${icon}" class="icon">`;
-  textContent.innerHTML = `<div class="weatherCondition">
+  if (!data.hasOwnProperty("error")) {
+    const icon = data.current.condition.icon;
+    weathericon.innerHTML = `<img src="https:${icon}" class="icon">`;
+    textContent.innerHTML = `<div class="weatherCondition">
   <h2>${data.current.condition.text} ( ${data.current.temp_c} °C )</h2>
   </div>
   <div>
@@ -22,12 +21,14 @@ async function result(location) {
       <li>City: ${data.location.name}</li>	
       <li>Region: ${data.location.region}</li>	
       <li>Country: ${data.location.country} </li>	
-      <li>Humidity: ${data.current.humidity} </li>	
+      <li>Humidity: ${data.current.humidity}%</li>	
       <li>Wind: ${data.current.humidity} Km/H (${data.current.wind_dir})</li>	
 
     </ul>
   </div>`;
+  } else {
+    weathericon.innerHTML = "";
+    textContent.innerHTML = `<h1 style="color:red">${data.error.message}<br> Please Try Again</h1>`;
+  }
   console.log(data);
 }
-
-// result("usa");
